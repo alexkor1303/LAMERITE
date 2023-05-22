df897e8cba4e62b725a5e7503cd5bbe1ffc999ba
Alexkor <Alexkor1303@yandex.ru>
2023-05-22 16:22:57 +0300

    feat:burger menu done

---
+++ more.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burgerid").addEventListener("click", function () {
+    document.querySelector(".main").classList.toggle("open");
+  });
+});
